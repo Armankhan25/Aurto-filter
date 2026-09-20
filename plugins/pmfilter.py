542495653a6ed71ea1835f61a0fb0a702dd8e8f0
Armankhan25 <sonicxMusic25@gmail.com>
2026-09-20 09:29:58 +0530

    Refactor link generation for media messages

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1122,28 +1122,25 @@ async def cb_handler(client: Client, query: CallbackQuery):
             username = query.from_user.mention
             log_msg = await client.send_cached_media(chat_id=BIN_CHANNEL, file_id=file_id,)
             fileName = quote_plus(get_name(log_msg))
-            file_hash = get_hash(log_msg)
-            dreamx_stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={file_hash}"
-            dreamx_miniapp = f"https://t.me/{temp.U_NAME}?startapp=watch_{log_msg.id}_{file_hash}"
-            dreamx_download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={file_hash}"
+            dreamx_stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+            miniapp_link = f"https://t.me/{temp.U_NAME}?startapp=w_{str(log_msg.id)}_{get_hash(log_msg)}"
+            dreamx_download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
             await query.answer(MSG_ALRT)
             await asyncio.sleep(1)
             await log_msg.reply_text(
                 text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
                 link_preview_options=LinkPreviewOptions(is_disabled=True),
-                reply_markup=InlineKeyboardMarkup([
-                                                    [InlineKeyboardButton("🚀 ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ 🚀", url=dreamx_download),
-                                                     InlineKeyboardButton('🖥️ ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ 🖥️', url=dreamx_stream)],
-                                                    [InlineKeyboardButton('📱 ᴏᴘᴇɴ ɪɴ ᴍɪɴɪ ᴀᴘᴘ 📱', url=dreamx_miniapp)]
-                                                ]))
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ 🚀", url=dreamx_download),  # we download Link
+                                                    InlineKeyboardButton('🖥️ ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ 🖥️', url=dreamx_stream)], [InlineKeyboardButton('📱 OPEN IN MINI APP', url=miniapp_link)]])  # web stream Link
+            )
             dreamcinezone = await query.edit_message_reply_markup(
                 reply_markup=InlineKeyboardMarkup([
                     [
                         InlineKeyboardButton("🚀 ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ 🚀", url=dreamx_download),
                         InlineKeyboardButton('🖥️ ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ 🖥️', url=dreamx_stream)
                     ],
+                    [InlineKeyboardButton('📱 OPEN IN MINI APP', url=miniapp_link)],
                     [
-                        InlineKeyboardButton('📱 ᴏᴘᴇɴ ɪɴ ᴍɪɴɪ ᴀᴘᴘ 📱', url=dreamx_miniapp),
                         InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)
                     ]
                 ])
